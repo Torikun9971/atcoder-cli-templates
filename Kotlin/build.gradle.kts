15bf6eb37fa787082a61faf69f6af387b0a5aeaf
Torikun9971 <msn20220326@gmail.com>
2025-11-03 02:40:23 +0900

    :heavy_plus_sign: [Kotlin] Add AtCoder-compatible libraries

--- Kotlin/build.gradle.kts
+++ Kotlin/build.gradle.kts
@@ -1,21 +1,39 @@
 import org.gradle.internal.os.OperatingSystem
+import org.gradle.kotlin.dsl.invoke
 import org.gradle.kotlin.dsl.register
+import java.nio.file.Path
+import kotlin.io.path.Path
 
 plugins {
-    kotlin("jvm") version "2.2.10"
+    alias(libs.plugins.kotlin.jvm)
 }
 
 group = ""
 version = "1.0.0"
 
 val accCommand = if (OperatingSystem.current().isWindows) "acc.cmd" else "acc"
+val accConfigDir = accConfigDir()
 
 repositories {
     mavenCentral()
+
+    flatDir {
+        dirs(accConfigDir.resolve("atcoder-libs"))
+    }
 }
 
 dependencies {
+    optionalImplementation(libs.ac.library.java) {
+        atcoderLib("ac_library17").exists()
+    }
 
+    implementation(libs.kotlinx.collections.immutable)
+}
+
+kotlin {
+    val java_version = libs.versions.java.get().toInt()
+
+    jvmToolchain(java_version)
 }
 
 tasks.jar {
@@ -30,10 +48,6 @@ tasks.jar {
     )
 
     duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-}
-
-kotlin {
-    jvmToolchain(17)
 }
 
 tasks.register<Exec>("sampleJudge") {
@@ -62,4 +76,45 @@ tasks.register("submit") {
 
 tasks.register("s") {
     dependsOn("submit")
+}
+
+fun accConfigDir(): Path {
+    val process = try {
+        ProcessBuilder(accCommand, "config-dir")
+            .start()
+    } catch (e: Exception) {
+        throw GradleException("Failed to execute '$accCommand config-dir': ${e.message}")
+    }
+
+    val path = process.inputReader().use { it.readLine() }
+    val exitCode = process.waitFor()
+
+    process.destroyForcibly()
+    if (exitCode != 0) {
+        throw GradleException("Failed to obtain AtCoder CLI config directory.")
+    }
+
+    return Path(path)
+}
+
+fun atcoderLib(name: String): File {
+    return accConfigDir
+        .resolve("atcoder-libs")
+        .resolve("$name.jar")
+        .toFile()
+}
+
+fun DependencyHandlerScope.optionalImplementation(
+    dependencyNotation: Provider<MinimalExternalModuleDependency>,
+    condition: () -> Boolean
+): Dependency? {
+    val name = dependencyNotation.get().name
+
+    return if (condition()) {
+        logger.lifecycle("Adding optional dependency: $name")
+        implementation(dependencyNotation)
+    } else {
+        logger.lifecycle("Skipping optional dependency: $name (not found)")
+        null
+    }
 }
